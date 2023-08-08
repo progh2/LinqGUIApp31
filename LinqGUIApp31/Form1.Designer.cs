0f7892a8c786c24973a8318568a0755e4acfd639
Gihun Ham <ham@e-mirim.hs.kr>
2023-08-08 11:32:24 +0900

    예제 데이터 작성

--- LinqGUIApp31/Form1.Designer.cs
+++ LinqGUIApp31/Form1.Designer.cs
@@ -119,6 +119,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "3000 함기훈";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.productBindingSource1)).EndInit();
